dc78789241b07287edc9a186f7b38bab8aa313e1
KimHawon <dkckacka1@naver.com>
2025-01-16 14:54:09 +0900

    패키지 세팅

---
+++ Runtime/Script/Util/SingletonMonoBehaviour.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+namespace Engine.Util
+{
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    var newObject = new GameObject();
+                    newObject.name = typeof(T).Name;
+
+                    var ins = newObject.AddComponent<T>();
+
+                    instance = ins; 
+                }
+
+                return instance;
+            }
+        }
+
+        public static bool HasInstance
+        {
+            get
+            {
+                return instance is not null;
+            }
+        }
+
+        private static T instance;
+
+        private void Awake()
+        {
+            if (instance is null)
+            {
+                instance = GetComponent<T>();
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+
+            Initialized();
+        }
+
+        public virtual void Initialized() { }
+    }
+}
