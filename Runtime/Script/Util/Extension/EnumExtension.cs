61a140e44cfac50e44ba22f8d19848373b82d845
Kimhawon <dkckacka12@gmail.com>
2025-05-29 23:45:16 +0900

    Feat Enum 갯수와 다음 Enum으로 넘어가는 확장 메서드 작업

--- Runtime/Script/Util/Extension/EnumExtension.cs
+++ Runtime/Script/Util/Extension/EnumExtension.cs
@@ -11,5 +11,21 @@ namespace Engine.Util.Extension
                 action?.Invoke((T)type);
             }
         }
+
+        public static int GetEnumCount<T>() where T : System.Enum
+        {
+            return System.Enum.GetValues(typeof(T)).Length;
+        }
+        
+        public static T GetMoveNext<T>(this T source) where T : System.Enum
+        {
+            var array = System.Enum.GetValues(typeof(T));
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (source.Equals(array.GetValue(i)))
+                    return (T)array.GetValue(i + 1);
+            }
+            return (T)array.GetValue(0);
+        }
     }
 }
